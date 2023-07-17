6c993e07dfbdff1fc31ab0ea588389a6eb12f2ef
ABDULRAFIU MUBARAK I <rafiumubarak2019@gmail.com>
2023-07-18 00:31:14 +0100

    Started the App layout

--- app/index.js
+++ app/index.js
@@ -1,11 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
 
-const Home = ()=>{
-    return (
-        <View>
-            <Text> Home </Text>
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+import { ScrollView } from "react-native-gesture-handler";
+
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
         </View>
-    )
-}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
